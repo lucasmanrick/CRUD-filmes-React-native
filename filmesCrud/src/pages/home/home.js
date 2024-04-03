b8affb063bb60dcd853872a1dd179f3232a14b22
Lucas Manrick <lucasmanrick.ipms@gmail.com>
2024-04-02 21:05:10 -0300

    layout home estatico

--- filmesCrud/src/pages/home/home.js
+++ filmesCrud/src/pages/home/home.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, View, Image, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { DatabaseConnection } from '../../database/database';
 
@@ -9,30 +9,30 @@ const db = new DatabaseConnection.getConnection
 export default function Home() {
   const navigation = useNavigation();
 
-//   const deleteDatabase = () => {
-//     db.transaction(tx => {
-//       tx.executeSql(
-//         "SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'", //seleciona o nome de todas tabelas que temos no sqlite, exceto tabelas que começam com sqlite_,
-//         [],
-//         (_,{rows}) => {
-//           rows._array.forEach((el) => {
-//             tx.executeSql(`Drop table if exists ${el.name}`,
-//             [],
-//             ()=> {
-//               console.log('tabela ${el.name} excluida com sucesso')
-              
-//             },
-//             (_,error) => {
-//               console.error(error)
-//             }
-//             )
-//           })
-//         }
-//       )
-//     })
-//   }
+  //   const deleteDatabase = () => {
+  //     db.transaction(tx => {
+  //       tx.executeSql(
+  //         "SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'", //seleciona o nome de todas tabelas que temos no sqlite, exceto tabelas que começam com sqlite_,
+  //         [],
+  //         (_,{rows}) => {
+  //           rows._array.forEach((el) => {
+  //             tx.executeSql(`Drop table if exists ${el.name}`,
+  //             [],
+  //             ()=> {
+  //               console.log('tabela ${el.name} excluida com sucesso')
 
-// deleteDatabase()
+  //             },
+  //             (_,error) => {
+  //               console.error(error)
+  //             }
+  //             )
+  //           })
+  //         }
+  //       )
+  //     })
+  //   }
+
+  // deleteDatabase()
 
   useEffect(() => {
     db.transaction(tx => {
@@ -51,19 +51,19 @@ export default function Home() {
   }, [])
 
 
-  
+
 
   const limpaTable = () => {
     db.transaction(tx => {
       tx.executeSql('DELETE FROM filmes;',
-      [],
-      (_,{rows}) => {
-        console.log(rows)
-        // setDataReceive(all)
-      },
-      (_,error)=>{
-        console.error(error)
-      });
+        [],
+        (_, { rows }) => {
+          console.log(rows)
+          // setDataReceive(all)
+        },
+        (_, error) => {
+          console.error(error)
+        });
     })
   }
 
@@ -88,34 +88,54 @@ export default function Home() {
   useEffect(() => {
     db.transaction(tx => {
       tx.executeSql('SELECT * FROM filmes',
-      [],
-      (_,{rows}) => {
-        console.log(rows)
-        // setDataReceive(all)
-      }, 
-      (_,error)=>{
-        console.error(error)
-      });
+        [],
+        (_, { rows }) => {
+          console.log(rows)
+          // setDataReceive(all)
+        },
+        (_, error) => {
+          console.error(error)
+        });
     })
   }, [])
 
   return (
     <View style={styles.container}>
-      <View style={{ width: '100%', height: '50%', display: 'flex', justifyContent: 'flex-end', paddingBottom: 25 }}>
-    
-        <Text>Seja bem vindo ao MovieLegacy</Text>
-      </View>
-      {/*container buttons navigators */}
-      <View style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'space-around' }}>
-        <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate('cadastrarFilme')}>
-          <Text style={{ color: 'white', fontWeight: 'bold' }}>Cadastre seu filme</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate('todosFilmes')}>
-          <Text style={{ color: 'white', fontWeight: 'bold' }}>Filmes cadastrados</Text>
-        </TouchableOpacity>
-      </View>
+      <ScrollView>
+        <View style={{ width: '100%', height: 400, display: 'flex', justifyContent: 'flex-end', paddingBottom: 25, position: 'relative' }}>
+          <Image style={{ width: '100%', height: '100%', position: 'absolute', zIndex: 1 }} source={require('../../../assets/poderosoThiefao.webp')} />
+          <Text style={{ position: 'absolute', zIndex: 4, color: 'white', fontWeight: 'bold', fontSize: 25, paddingBottom: 15 }}>Seja bem vindo ao MovieLegacy</Text>
+        </View>
+        {/*container buttons navigators */}
+        <View style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'space-around', marginTop: 20, marginBottom:20 }}>
+          <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate('cadastrarFilme')}>
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>Cadastre seu filme</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate('todosFilmes')}>
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>Filmes cadastrados</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{padding:5,width: '100%', height: 200, borderColor: 'black', borderWidth: 1, display: 'flex',overflow:'scroll' ,flexDirection: 'row', justifyContent:'space-between' }}>
+          <ScrollView horizontal={true}>
+            <View style={{ width: 200, height: '100%', margin:2 }}>
+              <Image style={{width:'100%',height:200}} source={require('../../../assets/poderosoThiefao.webp')}/>
+            </View>
+            <View style={{ width: 200, height: '100%', margin:2 }}>
+              <Image style={{width:'100%',height:200}} source={require('../../../assets/poderosoThiefao.webp')}/>
+            </View>
+            <View style={{ width: 200, height: '100%', margin:2 }}>
+              <Image style={{width:'100%',height:200}} source={require('../../../assets/poderosoThiefao.webp')}/>
+            </View>
+            <View style={{ width: 200, height: '100%', margin:2 }}>
+              <Image style={{width:'100%',height:200}} source={require('../../../assets/poderosoThiefao.webp')}/>
+            </View>
+          </ScrollView>
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
@@ -127,6 +147,7 @@ const styles = StyleSheet.create({
   },
   buttons: {
     width: '40%',
+    height: 50,
     backgroundColor: 'dodgerblue',
     padding: 5,
     display: 'flex',
